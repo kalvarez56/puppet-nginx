2b9ffbce514d9cac962424dbf268b2b95d7020c8
kalvarez56 <kalvarez56@gmail.com>
2017-06-02 19:34:15 +0000

    Add templates

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -23,9 +23,10 @@ define nginx::vhost(
   }
 
   file { $vhost_docroot:
-    ensure => directory,
-    owner  => $owner,
-    group  => $group,
-    mode   => '0775',
+    ensure  => directory,
+    recurse => true,
+    owner   => $owner,
+    group   => $group,
+    mode    => '0755',
   }
 }
